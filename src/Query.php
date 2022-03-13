7aaa6ec94e828d2ceccf635c6042a6ca420d2bc1
Leo Flapper <dominicus1991@gmail.com>
2022-03-13 17:47:42 +0100

    Added helper function to get query with bindings

---
+++ src/Query.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wefabric\Support;
+
+class Query
+{
+
+    /**
+     * Combines SQL and its bindings
+     *
+     * @param \Eloquent $query
+     * @return string
+     */
+    public static function getEloquentSqlWithBindings($query)
+    {
+        return vsprintf(str_replace('?', '%s', $query->toSql()), collect($query->getBindings())->map(function ($binding) {
+            return is_numeric($binding) ? $binding : "'{$binding}'";
+        })->toArray());
+    }
+}
